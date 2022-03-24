ab880150e4b0dbdebf62dd779a36de5eea2aadd7
NelsonTLima <nelsontlima@gmail.com>
2022-03-24 00:50:41 -0300

    Fixing things
    I fixed some bugs, cleared some unnecessary things, put an error counter and made comments to the script.

--- LTC_Analyzer.py
+++ LTC_Analyzer.py
@@ -1,13 +1,11 @@
-import sys
-import requests
-import json
-import concurrent.futures
+import requests, json, concurrent.futures
 from datetime import datetime
+from tqdm import tqdm
 from os import system
 from time import sleep
-from tqdm import tqdm
-
 system('clear') or None
+
+# After the libraries, we get all the static information. Starting date and counters will be before while.
 
 date = datetime.now()
 starting_time = date.strftime('%d/%m/%Y         %H:%M:%S')
@@ -17,6 +15,7 @@ investment = 0
 counter1 = 0
 counter2 = 0
 counter3 = 0
+error_counter = 0
 
 profit_list = []
 for i in profit_list:
@@ -27,6 +26,9 @@ main = 0
 with tqdm(total=6) as bar:
 	while main == 0:
 
+# After the while everything changes every second...
+
+# Lists and dictionaires.
 		ask_list = []
 		bid_list = []
 		dic_ask = {}
@@ -41,20 +43,20 @@ with tqdm(total=6) as bar:
                     'https://api.gateio.ws/api/v4/spot/order_book?currency_pair=LTC_USDT']
 
 		headers = {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                 			'User-Agent': 'aUserAgent'
-                }
+			'Accept': 'application/json',
+  			'Content-Type': 'application/json',
+  			'User-Agent': 'aUserAgent'
+		}
 
 		params = ''
 
-		def get_data(url, params):
+#Functions: the fist one is the standard funtion for every exchange to get data. The others are for geting the specific information we need (ask and bid).
+
+		def get_data(url, headers, params):
 			if url == urls[2]:
-				pass
-			else:
 				params = {'market': 'LTCUSDT', 'merge': '0.00001', 'limit': 1}
 			request = requests.request(
-				"GET", url, headers=headers, params=params, timeout=1.5)
+				"GET", url, headers=headers, params=params, timeout=2)
 			response = request.json()
 			return response
 
@@ -70,8 +72,8 @@ with tqdm(total=6) as bar:
 
 		def coinex():
 			data = (response['data'])
-			askslist = response['asks']
-			bidslist = response['bids']
+			askslist = data['asks']
+			bidslist = data['bids']
 			asks = (askslist[0])
 			ask = float(asks[0])
 			bids = (bidslist[0])
@@ -105,6 +107,8 @@ with tqdm(total=6) as bar:
 			bid = float(highestbid[0])
 			list_update(name, ask, bid)
 
+# These two next functions are for updating the lists and dictionaires.
+		
 		def list_update(name, ask, bid):
 			ask_list.append(ask)
 			bid_list.append(bid)
@@ -120,6 +124,8 @@ with tqdm(total=6) as bar:
 					local_list.append(key)
 			return local_list
 
+# These two are the main equations to the software. 
+		
 		def result():
 			total_amount = float(
 				(((investment - ltc_fee)*selling_price)/buying_price) - usdt_fee)
@@ -130,24 +136,33 @@ with tqdm(total=6) as bar:
 			amount = total_amount - investment
 			return percentage, amount
 
+# In the next lines we create 6 simultaneous threads for raising software speed and accuracy. Now we can accces the urls about 5 times faster.
+
 		with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
 			future_to_url = {executor.submit(
-				get_data, url, params): url for url in urls}
-			for future in tqdm(concurrent.futures.as_completed(future_to_url)):
+				get_data, url, headers, params): url for url in urls}
+			for future in concurrent.futures.as_completed(future_to_url):
 				url = future_to_url[future]
+				bar.update()
+
+# Now we need to put names for thracing where does the results of every "url" come from.
+
 				if url == urls[0]:
 					name = 'BINANCE'
 				elif url == urls[1]:
 					name = 'COINBASE'
 				elif url == urls[2]:
 					name = 'COINEX'
+					params = {'market': 'LTCUSDT', 'merge': '0.00001', 'limit': 1}
 				elif url == urls[3]:
 					name = 'CRYPTO.COM'
 				elif url == urls[4]:
 					name = 'FTX'
 				else:
 					name = 'GATEIO'
-				bar.update()
+
+# Now we execute the specific tasks for every exchange. I could be more economic if i have done it in the last step, but it would ruin the exception due to not naming the exchanges before "try".			
+
 				try:
 					response = future.result()
 					if url == urls[0]:
@@ -164,8 +179,9 @@ with tqdm(total=6) as bar:
 						gateio()
 				except:
 					FAIL.append(name)
-				else:
-					pass
+					error_counter += 1
+
+# Now we stablish variables and make some rules.
 
 		try:
 			selling_price = float(max(ask_list))
@@ -197,16 +213,18 @@ with tqdm(total=6) as bar:
 					profit_list.append(percentage)
 
 				if percentage >= 0.1:
-					counter1 = counter1 + 1
+					counter1 += 1
 
 				if percentage >= 0.15:
-					counter2 = counter2 + 1
+					counter2 += 1
 
 				if percentage >= 0.2:
 					counter3 = counter3 + 1
 
 				checkpoint = datetime.now()
 				check_time = checkpoint.strftime('%d/%m/%Y         %H:%M:%S')
+
+# And finaly we have the prints with all the outputs.
 
 				system('clear') or None
 
@@ -228,17 +246,18 @@ with tqdm(total=6) as bar:
 				print(f'Profit > 0.15 %: {counter2} times')
 				print(f'Profit > 0.20 %: {counter3} times')
 				print("Max profit: " + format(max(profit_list), '.2f') + str('%'))
-				if len(FAIL) == 1:
+				if len(FAIL) >= 1:
 					print("\nWe couldn't connect:")
 					for i in FAIL:
 						print(i)
-						if len(FAIL) > 1:
-							print(
-								f"\nWe couldn't contact {len(FAIL)} exchanges.\nCheck your connection.")
+				print(f'\nConnecting attempts failed {error_counter} times.')
 
 				print(f'\nLast checkpoint:\n{check_time}\n\n')
 
+# In case of there's no internet.
+
 		except:
+			error_counter -= 6
 			currentime = datetime.now()
 			current_time = currentime.strftime('%d/%m/%Y         %H:%M:%S')
 
